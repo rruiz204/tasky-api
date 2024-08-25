95ae190ff0c60523897b75f1f90375f09da2da6a
roney ruiz <roneyruizrojas@gmail.com>
2024-08-25 16:49:26 -0600

    setup and configure sqlalchemy

---
+++ app/database/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from app.configuration.orm import DATABASE_URL
+engine = create_engine(DATABASE_URL)
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+  db = session()
+  try:
+    yield db
+  finally:
+    db.close()
