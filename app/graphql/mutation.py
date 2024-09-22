0b0778d4f27b68d84d6552a17b3f6c5bce19a9bc
roney ruiz <roneyruizrojas@gmail.com>
2024-09-22 15:25:27 -0600

    remove old queries and mutations

--- app/graphql/mutation.py
+++
@@ -1,9 +0,0 @@
-import strawberry
-
-from app.graphql.schema.user import UserType
-
-from app.services.user_service import create_user
-
-@strawberry.type
-class Mutation:
-  create_user: UserType = strawberry.mutation(resolver=create_user)
