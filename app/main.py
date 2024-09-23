865d6de9fdb5e32f76618f6db537fed8ba48a825
roney ruiz <roneyruizrojas@gmail.com>
2024-09-22 18:02:02 -0600

    use new enhances

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-
 app = FastAPI()
 
 import strawberry
 from strawberry.fastapi import GraphQLRouter
-from app.graphql.query import Query
-from app.graphql.mutation import Mutation
+from app.graphql.queries.query import Query
+from app.graphql.mutations.mutation import Mutation
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema, graphiql=True)
