868dd83cb6d8d6c2847ee36fba159f8bc4b1a6cc
roney ruiz <roneyruizrojas@gmail.com>
2024-09-22 18:00:04 -0600

    modify create user schema

--- app/schemas/user/create_user_schema.py
+++ app/schemas/user/create_user_schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr, constr
 
-class UserInputModel(BaseModel):
-  username: constr(min_length=3, max_length=50)
+class CreateUserSchena(BaseModel):
+  username: constr(min_length=5, max_length=30)
   email: EmailStr
   password: constr(min_length=8)
