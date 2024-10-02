091e435ca2b664e9c8195f8a26e3f542d40c9c7c
roney ruiz <roneyruizrojas@gmail.com>
2024-10-01 22:38:38 -0600

    add typed to this components

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,14 +1,15 @@
+from typing import List
 from sqlalchemy.orm import Session
 from app.models.user import User
 
 class UserRepository:
-  def __init__(self, db: Session):
+  def __init__(self, db: Session) -> None:
     self.db = db
 
-  def get_users(self):
+  def get_users(self) -> List[User]:
     return self.db.query(User).all()
 
-  def create_user(self, user: User):
+  def create_user(self, user: User) -> User:
     self.db.add(user)
     self.db.commit()
     self.db.refresh(user)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,15 +1,16 @@
+from typing import List
 from app.database.unit_of_work import UnitOfWork
 from app.graphql.schema.user_types import UserInput
 from app.schemas.user.create_user_schema import CreateUserSchena
 from app.models.user import User
 
 class UserService:
-  def __init__(self):
+  def __init__(self) -> None:
     self.uow = UnitOfWork()
   
-  def get_all_users(self):
+  def get_all_users(self) -> List[User]:
     return self.uow.users.get_users();
 
-  def create_new_user(self, input: UserInput):
+  def create_new_user(self, input: UserInput) -> User:
     user = CreateUserSchena(**input.__dict__)
     return self.uow.users.create_user(User(username=user.username, email=user.email, password=user.password))
