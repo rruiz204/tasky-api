a79a2d5ffbca556ff02755eda9c4c4df1db605c8
roney ruiz <roneyruizrojas@gmail.com>
2024-10-01 22:39:19 -0600

    fix dependency injection

--- app/graphql/mutations/user_mutations.py
+++ app/graphql/mutations/user_mutations.py
@@ -2,12 +2,11 @@ import strawberry
 from app.services.user_service import UserService
 from app.graphql.schema.user_types import UserType, UserInput
 
-
 @strawberry.type
 class UserMutation():
-  service = UserService()
 
   @strawberry.mutation
   def create_user(self, input: UserInput) -> UserType:
-    user = self.service.create_new_user(input=input)
+    service = UserService()
+    user = service.create_new_user(input=input)
     return UserType(id=user.id, username=user.username, email=user.email)
--- app/graphql/queries/user_queries.py
+++ app/graphql/queries/user_queries.py
@@ -5,9 +5,9 @@ from app.graphql.schema.user_types import UserType
 
 @strawberry.type
 class UserQuery:
-  service = UserService()
 
   @strawberry.field
   def users(self) -> List[UserType]: 
-    users = self.service.get_all_users()
+    service = UserService()
+    users = service.get_all_users()
     return [UserType(id=user.id, username=user.username, email=user.email) for user in users]
