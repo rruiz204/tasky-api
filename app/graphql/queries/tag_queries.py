8f426926d9172d1a651c3aaf45948897640e262c
roney ruiz <roneyruizrojas@gmail.com>
2024-10-06 23:31:52 -0600

    add authentication to these queries

--- app/graphql/queries/tag_queries.py
+++ app/graphql/queries/tag_queries.py
@@ -2,11 +2,12 @@ import strawberry
 from typing import List
 from app.services.tag_service import TagService
 from app.graphql.schema.tag_types import TagType
+from app.graphql.permissions.is_authenticated import IsAuthenticated
 
 @strawberry.type
 class TagQuery:
 
-  @strawberry.field
+  @strawberry.field(permission_classes=[IsAuthenticated])
   def tags(self) -> List[TagType]: 
     service = TagService()
     tags = service.get_all_tags()
--- app/graphql/queries/user_queries.py
+++ app/graphql/queries/user_queries.py
@@ -2,11 +2,12 @@ import strawberry
 from typing import List
 from app.services.user_service import UserService
 from app.graphql.schema.user_types import UserType
+from app.graphql.permissions.is_authenticated import IsAuthenticated
 
 @strawberry.type
 class UserQuery:
 
-  @strawberry.field
+  @strawberry.field(permission_classes=[IsAuthenticated])
   def users(self) -> List[UserType]: 
     service = UserService()
     users = service.get_all_users()
