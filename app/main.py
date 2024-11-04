0da26860eeba543248a0b9d35f4f2b0a89f9f569
roney ruiz <roneyruizrojas@gmail.com>
2024-11-03 20:05:39 -0600

    feat: configure cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
 from strawberry.fastapi import GraphQLRouter
 from app.graphql.config import StrawberrySchema
 
